312fbbea5db62b6357c65e8ad51660dcac137e27
Yeondam Park <giyen9088@gmail.com>
2018-05-15 20:59:20 -0700

    setting up class block

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,36 @@ import * as CryptoJS from "crypto-js";
 class Block {
   static calculateBlockHash = (
     index: number,
-    previouseHash: string,
+    previousHash: string,
     timestamp: number,
     data: string
   ): string => {
-    return CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   };
 
+  static validateStructure = (block: Block): boolean =>
+    typeof block.index === "number" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timestamp === "number" &&
+    typeof block.data === "string";
+
   public index: number;
-  public;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
